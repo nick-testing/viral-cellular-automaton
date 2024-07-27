663f5bf1a232128d62fa224341c6bd1256189896
Nick <nick@example.com>
2024-07-27 22:21:49 +0300

    Updated classes API

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,71 +1,41 @@
-#include "include.h"
+#include "classes.h"
 
 using namespace std;
-
-class Cell {
-public:
-    tuple<int, int> location;
-    int speed;
-    Cell(tuple<int, int> coor, int speed): location(coor), speed(speed){};
-    std::vector<std::tuple<int, int>> get_neighbours(int radius);
-    virtual void next_iteration(int index)=0;
-};
-
-
-struct matrixData {
-    Cell* cell = nullptr;
-    bool is_occupied = false;
-    bool is_healthy = false;
-    bool is_sick = false;
-    bool is_immune = false;
-};
 
 matrixData adj_mat[200][200];
 
 
-class HealthyCell : public Cell {
-public:
-    HealthyCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
+Cell::Cell(tuple<int, int> coor, int speed): location(coor), speed(speed){};
+
+HealthyCell::HealthyCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
         int x = get<0>(coor);
         int y = get<1>(coor);
         adj_mat[x][y].cell = this;
         adj_mat[x][y].is_occupied = true;
         adj_mat[x][y].is_healthy = true;
     };
-    void next_iteration(int index) override;
 
+SickCell::SickCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
+    int x = get<0>(coor);
+    int y = get<1>(coor);
+    adj_mat[x][y].cell = this;
+    adj_mat[x][y].is_occupied = true;
+    adj_mat[x][y].is_sick = true;
 };
 
-class SickCell : public Cell{
-    int generation = 1;
-public:
-    SickCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
-        int x = get<0>(coor);
-        int y = get<1>(coor);
-        adj_mat[x][y].cell = this;
-        adj_mat[x][y].is_occupied = true;
-        adj_mat[x][y].is_sick = true;
-    };
-    SickCell(tuple<int, int> coor, int speed, int gen): Cell(coor, speed), generation(gen) {
-        int x = get<0>(coor);
-        int y = get<1>(coor);
-        adj_mat[x][y].cell = this;
-        adj_mat[x][y].is_occupied = true;
-        adj_mat[x][y].is_sick = true;
-    };
-    void next_iteration(int index) override;
+SickCell::SickCell(tuple<int, int> coor, int speed, int gen): Cell(coor, speed), generation(gen) {
+    int x = get<0>(coor);
+    int y = get<1>(coor);
+    adj_mat[x][y].cell = this;
+    adj_mat[x][y].is_occupied = true;
+    adj_mat[x][y].is_sick = true;
 };
 
-class ImmuneCell : public Cell {
-public:
-    ImmuneCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
-        int x = get<0>(coor);
-        int y = get<1>(coor);
-        adj_mat[x][y].cell = this;
-        adj_mat[x][y].is_occupied = true;
-        adj_mat[x][y].is_immune = true;
-    };
-    void next_iteration(int index) override;
-
+ImmuneCell::ImmuneCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
+    int x = get<0>(coor);
+    int y = get<1>(coor);
+    adj_mat[x][y].cell = this;
+    adj_mat[x][y].is_occupied = true;
+    adj_mat[x][y].is_immune = true;
 };
 
---
+++ src/classes.h
@@ -0,0 +1,47 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+#include <vector>
+#include <tuple>
+
+class Cell {
+public:
+    std::tuple<int, int> location;
+    int speed;
+    Cell(std::tuple<int, int> coor, int speed);
+    std::vector<std::tuple<int, int>> get_neighbours(int radius);
+    virtual void next_iteration(int index)=0;
+};
+
+class HealthyCell : public Cell {
+public:
+    HealthyCell(std::tuple<int, int> coor, int speed);
+    void next_iteration(int index) override;
+
+};
+
+class SickCell : public Cell{
+    int generation = 1;
+public:
+    SickCell(std::tuple<int, int> coor, int speed);
+    SickCell(std::tuple<int, int> coor, int speed, int gen);
+    void next_iteration(int index) override;
+};
+
+class ImmuneCell : public Cell {
+public:
+    ImmuneCell(std::tuple<int, int> coor, int speed);
+    void next_iteration(int index) override;
+
+};
+
+struct matrixData {
+    Cell* cell = nullptr;
+    bool is_occupied = false;
+    bool is_healthy = false;
+    bool is_sick = false;
+    bool is_immune = false;
+};
+
+
+#endif // CLASSES_H
