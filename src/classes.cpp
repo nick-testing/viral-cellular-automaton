446c5848ed986a1f1d8bf6cf2523bc3f61053a1f
Nick <nick@example.com>
2024-07-27 22:24:18 +0300

    Appended class functions

--- src/classes.cpp
+++ src/classes.cpp
@@ -39,3 +39,193 @@ ImmuneCell::ImmuneCell(tuple<int, int> coor, int speed): Cell(coor, speed) {
     adj_mat[x][y].is_immune = true;
 };
 
+
+/*****************************************************************************
+ ****************************** Object Functions *****************************
+ *****************************************************************************/
+
+/**
+ * Returns a vector containing neighbor coordinates for the current cell
+ *  Neigbors of the middle cell are represented as:
+ *  1 2 3
+ *  4   5
+ *  6 7 8  
+ * @param radius proximity to neighbors
+ * @return tuple
+ */
+std::vector<std::tuple<int, int>> Cell::get_neighbours(int radius) {
+    std::vector<std::tuple<int, int>> output;
+    int x = get<0>(location);
+    int y = get<1>(location);
+       
+    if (x > radius && y > radius && x < 199 - radius && y < 199 - radius) {
+        output.push_back(tuple<int, int>(x - 1 - radius, y - 1 - radius));      // 1
+        output.push_back(tuple<int, int>(x - 1 - radius, y));                   // 2
+        output.push_back(tuple<int, int>(x - 1 - radius, y + 1 + radius));      // 3
+        output.push_back(tuple<int, int>(x, y - 1 - radius));                   // 4
+        output.push_back(tuple<int, int>(x, y + 1 + radius));                   // 5
+        output.push_back(tuple<int, int>(x + 1 + radius, y - 1 - radius));      // 6
+        output.push_back(tuple<int, int>(x + 1 + radius, y));                   // 7
+        output.push_back(tuple<int, int>(x + 1 + radius, y + 1 + radius));      // 8
+        return output;
+    }
+    else if (x <= radius && x >= 0) {
+        output.push_back(tuple<int, int>(199 - radius + x, y));              // 2
+        output.push_back(tuple<int, int>(x + 1 + radius, y));            // 7
+
+        // only top is wrapping
+        if (y > radius && y < 199 - radius){
+            output.push_back(tuple<int, int>(199 - radius + x, y - 1 - radius));  // 1
+            output.push_back(tuple<int, int>(199 - radius + x, y + 1 +radius));  // 3
+            output.push_back(tuple<int, int>(x, y - 1 - radius));    // 4
+            output.push_back(tuple<int, int>(x, y + 1 + radius));    // 5
+            output.push_back(tuple<int, int>(x + 1 + radius, y - 1 - radius));    // 6
+            output.push_back(tuple<int, int>(x + 1 + radius, y + 1 + radius));    // 8
+            return output;
+        }
+
+        //top and right are wrapping
+        else if (y >= 199 - radius && y < 200) {
+            output.push_back(tuple<int, int>(199 - radius + x, y - 1 - radius));  // 1
+            output.push_back(tuple<int, int>(199 - radius + x, radius - (199 - y)));      // 3
+            output.push_back(tuple<int, int>(x, y - 1 - radius));    // 4
+            output.push_back(tuple<int, int>(x, radius - (199 - y)));        // 5
+            output.push_back(tuple<int, int>(x + 1 + radius, y - 1 - radius));    // 6
+            output.push_back(tuple<int, int>(x + 1 + radius, radius - (199 - y)));        // 8
+            return output;
+        }
+
+            // top and left are wrapping.
+
+        else if (y <= radius && y >= 0) {
+            output.push_back(tuple<int, int>(199 - radius + x, 199 - radius + y));    // 1
+            output.push_back(tuple<int, int>(199 - radius + x, y + 1 + radius ));      // 3
+            output.push_back(tuple<int, int>(x, 199 - radius + y));      // 4
+            output.push_back(tuple<int, int>(x, y + 1 + radius));        // 5
+            output.push_back(tuple<int, int>(x + 1 + radius, 199 - radius + y));      // 6
+            output.push_back(tuple<int, int>(x + 1 + radius, y + 1 + radius));        // 8
+            return output;
+        }
+    }
+    // bottom is wrapping
+    else if (x >= 199 - radius && x < 200) {
+        output.push_back(tuple<int, int>(x - 1 - radius, y));              // 2
+        output.push_back(tuple<int, int>(x - 199 + radius, y));                // 7
+
+        // only bottom row is wrapping
+        if (y > radius && y < 199 - radius) {
+            output.push_back(tuple<int, int>(x - 1 - radius, y - 1 - radius));  // 1
+            output.push_back(tuple<int, int>(x - 1 - radius, y + 1 + radius));  // 3
+            output.push_back(tuple<int, int>(x, y - 1 - radius));    // 4
+            output.push_back(tuple<int, int>(x, y + 1 + radius));    // 5
+            output.push_back(tuple<int, int>(x - 199 + radius, y - 1 - radius));    // 6
+            output.push_back(tuple<int, int>(x - 199 + radius, y + 1 + radius));    // 8
+            return output;
+        }
+
+        //bottom right is wrapping
+        else if (y >= 199 - radius && y < 200) {
+            output.push_back(tuple<int, int>(x - 1 - radius, y - 1 - radius));  // 1
+            output.push_back(tuple<int, int>(x - 1 - radius, y - 199 + radius));      // 3
+            output.push_back(tuple<int, int>(x, y - 1 - radius));  // 4
+            output.push_back(tuple<int, int>(x, y - 199 + radius));      // 5
+            output.push_back(tuple<int, int>(x - 199 + radius, y - 1 - radius));    // 6
+            output.push_back(tuple<int, int>(x - 199 + radius, y - 199 + radius));        // 8
+            return output;
+        }
+
+            // bottom left is wrapping
+        else if (y <= radius && y >= 0) {
+            output.push_back(tuple<int, int>(x - 1 - radius, 199 - radius + y));    // 1
+            output.push_back(tuple<int, int>(x - 1 - radius, y + 1 + radius));  // 3
+            output.push_back(tuple<int, int>(x, 199 - radius + y));    // 4
+            output.push_back(tuple<int, int>(x, y + 1 + radius));  // 5
+            output.push_back(tuple<int, int>(x - 199 + radius, 199 - radius + y));      // 6
+            output.push_back(tuple<int, int>(x - 199 + radius, y + 1 + radius));        // 8
+            return output;
+        }
+    }
+    // only left is wrapping
+    else if (y <= radius  && y >= 0) {
+        output.push_back(tuple<int, int>(x - 1 - radius, 199 - radius + y));      // 1
+        output.push_back(tuple<int, int>(x - 1 - radius, y));           // 2
+        output.push_back(tuple<int, int>(x - 1 - radius, y + 1 + radius));   // 3
+        output.push_back(tuple<int, int>(x, 199 - radius + y));             // 4
+        output.push_back(tuple<int, int>(x, y + 1 + radius));            // 5
+        output.push_back(tuple<int, int>(x + 1 + radius, 199 - radius + y));      // 6
+        output.push_back(tuple<int, int>(x + 1 + radius, y));            // 7
+        output.push_back(tuple<int, int>(x + 1 + radius, y + 1 + radius));     // 8
+        return output;
+    }
+    // only right is wrapping
+    else if (y >= 199 - radius && y < 200) {
+        output.push_back(tuple<int, int>(x - 1 - radius, y - 1 - radius));      // 1
+        output.push_back(tuple<int, int>(x - 1 - radius, y));           // 2
+        output.push_back(tuple<int, int>(x - 1 - radius, y - 199 + radius));   // 3
+        output.push_back(tuple<int, int>(x, y - 1 - radius));             // 4
+        output.push_back(tuple<int, int>(x, y - 199 + radius));            // 5
+        output.push_back(tuple<int, int>(x + 1 + radius, y - 1 - radius));      // 6
+        output.push_back(tuple<int, int>(x + 1 + radius, y));            // 7
+        output.push_back(tuple<int, int>(x + 1 + radius, y - 199 + radius));     // 8
+        return output;
+    } else
+        output.push_back(get_random_coordinates());
+    return output;
+}
+
+
+/**
+ * Performs one iteration step for the healthy cell that was called from.
+ * @param index
+ */
+void HealthyCell::next_iteration(int index) {
+    int x = get<0>(location);
+    int y = get<1>(location);
+
+    bool sick_next_iteration = is_cell_sick(this);
+    auto all_neighbor_coordinates = get_neighbours(speed);
+    auto new_cell_coordinate = choose_random_neighbor(all_neighbor_coordinates, location);
+
+    // Freeing up current location, so that another cell can occupy it.
+    free_cell(x, y);
+    // create a new Healthy cell
+    if (!sick_next_iteration) {
+        cell_array.at(index) = new HealthyCell(new_cell_coordinate, speed);
+    }
+    // Create a new sick cell
+    else {
+        healthy_counter--;
+        temp_sick_counter++;
+        cell_array.at(index) = new SickCell(new_cell_coordinate, speed);
+    }
+    delete this;
+}
+
+void SickCell::next_iteration(int index) {
+    auto all_neighbor_coordinates = get_neighbours(speed);
+    auto new_cell_coordinate = choose_random_neighbor(all_neighbor_coordinates, location);
+    int x = get<0>(location);
+    int y = get<1>(location);
+    // Freeing up current location, so that another cell can occupy it.
+    free_cell(x, y);
+    if (generation < X) {
+        cell_array.at(index) = new SickCell(new_cell_coordinate, speed, generation + 1);
+    } else {
+        cell_array.at(index) = new ImmuneCell(new_cell_coordinate, speed);
+        temp_sick_counter--;
+        immune_counter++;
+    }
+    delete this;
+}
+
+void ImmuneCell::next_iteration(int index) {
+    auto all_neighbor_coordinates = get_neighbours(speed);
+    auto new_cell_coordinate = choose_random_neighbor(all_neighbor_coordinates, location);
+    int x = get<0>(location);
+    int y = get<1>(location);
+    // Freeing up current location, so that another cell can occupy it.
+    free_cell(x, y);
+    cell_array.at(index) = new ImmuneCell(new_cell_coordinate, speed);
+    delete this;
+}
+
